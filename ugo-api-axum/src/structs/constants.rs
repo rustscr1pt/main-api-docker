bdb230e05a46c6d7351b4a801a6fe64085630d8e
rust_lang_lover <rust_lang_lover@proton.me>
2024-09-19 12:49:33 +0300

    // commented session_duration() + refactored env::var() extractors

--- ugo-api-axum/src/structs/constants.rs
+++ ugo-api-axum/src/structs/constants.rs
@@ -22,31 +22,103 @@ pub const STANDARD_IP : &'static str = "0.0.0.0"; // standard IP for deploying
 
 pub const BOT_CHAT_ID : &'static str = "-1002156530519";
 
-pub fn SESSION_DURATION() -> u16 {
-    return env::var("SESSION_DURATION").unwrap().parse::<u16>().unwrap()
-}
+// pub fn SESSION_DURATION() -> u16 {
+//     match env::var("SESSION_DURATION") {
+//         Ok(session_duration) => {
+//             match session_duration.parse::<u16>() {
+//                 Ok(session_duration) => {
+//                     return session_duration
+//                 }
+//                 Err(err) => {
+//                     println!("{}", err);
+//                     return 900
+//                 }
+//             }
+//         }
+//         Err(err) => {
+//             println!("SESSION_DURATION => {}", err);
+//             return 900
+//         }
+//     }
+// }
 
 pub fn DEPLOY_PORT() -> u16 {
-    return env::var("DEPLOY_PORT").unwrap().parse::<u16>().unwrap()
+    match env::var("DEPLOY_PORT") {
+        Ok(deploy_port) => {
+            match deploy_port.parse::<u16>() {
+                Ok(deploy_port) => {
+                    return deploy_port
+                }
+                Err(err) => {
+                    println!("{}", err);
+                    return 8000
+                }
+            }
+        }
+        Err(err) => {
+            println!("DEPLOY_PORT => {}", err);
+            return 8000
+        }
+    }
 }
 
 pub fn FILE_LOCATION() -> String {
-    return String::from(env::var("FILE_LOCATION").unwrap())
+    match env::var("FILE_LOCATION") {
+        Ok(file_location) => {
+            return String::from(file_location)
+        }
+        Err(err) => {
+            println!("FILE_LOCATION => {}", err);
+            return String::from("mysql.txt")
+        }
+    }
 }
 
 pub fn BOT_TOKEN_LOCATION() -> String {
-    return String::from(env::var("BOT_TOKEN_LOCATION").unwrap())
+    match env::var("BOT_TOKEN_LOCATION") {
+        Ok(bot_token_location) => {
+            return String::from(bot_token_location)
+        }
+        Err(err) => {
+            println!("BOT_TOKEN_LOCATION => {}", err);
+            return String::from("bot_token.txt")
+        }
+    }
 }
 
 pub fn TG_EXPRESS_API_PORT() -> String {
-    return String::from(env::var("TELEGRAM_API_PORT").unwrap())
+    match env::var("TELEGRAM_API_PORT") {
+        Ok(telegram_api_port) => {
+            return String::from(telegram_api_port)
+        }
+        Err(err) => {
+            println!("TG_EXPRESS_API_PORT => {}", err);
+            return String::from("0")
+        }
+    }
 }
 
 pub fn AUTH_EXPRESS_API_PORT() -> String {
-    return String::from(env::var("AUTH_EXPRESS_API_PORT").unwrap())
+    match env::var("AUTH_EXPRESS_API_PORT") {
+        Ok(auth_express_api_port) => {
+            return String::from(auth_express_api_port)
+        }
+        Err(err) => {
+            println!("AUTH_EXPRESS_API_PORT => {}", err);
+            return String::from("1")
+        }
+    }
 }
 
 pub fn TOKEN_EXPRESS_API_PORT() -> String {
-    return String::from(env::var("TOKEN_EXPRESS_API_PORT").unwrap())
+    match env::var("TOKEN_EXPRESS_API_PORT") {
+        Ok(token_express_api_port) => {
+            return String::from(token_express_api_port)
+        }
+        Err(err) => {
+            println!("TOKEN_EXPRESS_API_PORT => {}", err);
+            return String::from("2")
+        }
+    }
 }
 
