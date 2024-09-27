dfff544a7ac884299ddfd27e1fc5a9972ad0db1a
rust_lang_lover <rust_lang_lover@proton.me>
2024-09-27 11:26:36 +0300

    refactored init_db in modules so connection is kept alive

--- tg-express-api/mysql/init_db.js
+++ tg-express-api/mysql/init_db.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql2');
 const {mysql_password} = require("../structs/constants");
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
     port: "3306",
     user: "gen_user",
     host: "94.241.169.12",
     database: "default_db",
-    password: mysql_password()
+    password: mysql_password(),
+    waitForConnections: true,
+    connectionLimit: 10
 });
 
 mysqlConnection.connect((err) => {
