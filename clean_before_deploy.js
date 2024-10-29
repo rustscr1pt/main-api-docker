7df5a08ff351f7587dc6782a3c8f448e98415ebf
rust_lang_lover <rust_lang_lover@proton.me>
2024-10-29 13:25:52 +0300

    // added comments

--- clean_before_deploy.js
+++ clean_before_deploy.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const {exec} = require('child_process');
 
+// Check for node_modules directories in project folders.
 function remove_node_modules(dirPath) {
     fs.readdir(dirPath, (err, files) => {
         if (err) {
