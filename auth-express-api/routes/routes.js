d16c6954a50fedf1af8087c3f942abad23241e70
rust_lang_lover <rust_lang_lover@proton.me>
2024-10-30 11:37:15 +0300

    // removed check route

--- auth-express-api/routes/routes.js
+++ auth-express-api/routes/routes.js
@@ -7,9 +7,6 @@ const routes = express.Router();
 routes
     .post('/authenticate/', (req, res) => {
         authenticate_runtime(req.body, res, mysqlConnection)
-    })
-    .get('/is_available/', (req, res) => {
-        res.status(200).send("OK")
-    })
+    });
 
 module.exports = routes;
