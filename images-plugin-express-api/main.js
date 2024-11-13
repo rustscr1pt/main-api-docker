fae83f4f7c20bfa5d904eeec3f54b53c0fb17b02
Egor Ivanov <rust_lang_lover@proton.me>
2024-11-13 23:57:22 +0300

    // added keep-alive mechanism to images-plugin-express-api

--- images-plugin-express-api/main.js
+++ images-plugin-express-api/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const core_router = require('./routes/core_router');
 const {static_path} = require("./structs/constants");
+const fetch = require('node-fetch');
 const {port} = require('./structs/constants');
 
 app.use(express.json());
@@ -10,8 +11,20 @@ app.use(express.json());
 app.use(static_path);
 
 // Add router
-app.use(core_router)
+app.use(core_router);
+
+// Route for keeping the module alive
+app.get('/health/', (req, res) => {
+    res.status(200).send('OK');
+});
 
 app.listen(port, () => {
     console.log(`Images Plugin Express Api is running at http://localhost:${port}`);
 });
+
+// Keep alive mechanism
+setInterval(() => {
+    fetch(`http://localhost:${port}/health/`)
+        .then(res => console.log('Keep-alive ping successful:', res.status))
+        .catch(err => console.error('Keep-alive ping failed:', err));
+}, 300000); // Every 5 minutes (300,000 ms)
--- token-express-api/main.js
+++ token-express-api/main.js
@@ -5,13 +5,13 @@ const fetch = require('node-fetch');
 
 const port = process.env.DEPLOY_PORT || 8002;
 
+app.use(express.json());
+app.use(routes);
+
 // Route for keeping the module alive
 app.get('/health/', (req, res) => {
     res.status(200).send('OK');
 });
-
-app.use(express.json());
-app.use(routes);
 
 app.listen(port, function() {
     console.log(`Express token API started at http://localhost:${port}`)
@@ -19,7 +19,7 @@ app.listen(port, function() {
 
 // Keep alive mechanism
 setInterval(() => {
-    fetch(`http://localhost:${port}/health`)
+    fetch(`http://localhost:${port}/health/`)
         .then(res => console.log('Keep-alive ping successful:', res.status))
         .catch(err => console.error('Keep-alive ping failed:', err));
 }, 300000); // Every 5 minutes (300,000 ms)
