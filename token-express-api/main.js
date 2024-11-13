e8701128a6c8d746e48176faed2420892b0014be
Egor Ivanov <rust_lang_lover@proton.me>
2024-11-13 23:49:45 +0300

    // added keep-alive mechanism to token-express-api

--- token-express-api/main.js
+++ token-express-api/main.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes/routes');
+const fetch = require('node-fetch');
 
 const port = process.env.DEPLOY_PORT || 8002;
+
+app.get('/health/', (req, res) => {
+    res.status(200).send('OK');
+});
 
 app.use(express.json());
 app.use(routes);
@@ -10,3 +15,10 @@ app.use(routes);
 app.listen(port, function() {
     console.log(`Express token API started at http://localhost:${port}`)
 });
+
+// Keep alive mechanism
+setInterval(() => {
+    fetch(`http://localhost:${port}/health`)
+        .then(res => console.log('Keep-alive ping successful:', res.status))
+        .catch(err => console.error('Keep-alive ping failed:', err));
+}, 300000); // Every 5 minutes (300,000 ms)
